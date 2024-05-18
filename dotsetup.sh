b9928a94fded4497e0679cab75020f9ad8a46aa3
swaroopanand <swaroopanand@email.com>
2024-05-18 10:53:09 +0530

    fixed type in dotsetup

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com:swaroopanand10/dotstow.git "$HOME/dotstow"
+git clone https://github.com/swaroopanand10/dotstow.git "$HOME/dotstow"
 
 ## doing it like this so that stow does not happen if the directory of dotstow does not get clone properly for some reason
 cd "$HOME/dotstow/" && stow */ -t ~ --adopt
