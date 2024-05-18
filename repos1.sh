a1d7c4a69b8a28a536de6e59c54248927986126b
swaroopanand <swaroopanand@email.com>
2024-05-18 12:43:49 +0530

    fixed error in repos

--- repos1.sh
+++ repos1.sh
@@ -3,4 +3,4 @@
 ### This file only contains essential parts
 
 git clone git@github.com:swaroopanand10/lvim.git $HOME/.config/nvim
-git clone git@github.com:swaroopanand10/autoinstall.git $HOME/.config/autoinstall
+git clone git@github.com:swaroopanand10/autosetup.git $HOME/.config/autosetup
