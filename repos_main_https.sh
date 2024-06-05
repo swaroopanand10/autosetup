d2820b81d37410446fc36d988c6d5ca6021cde4b
swaroopanand <swaroopanand@email.com>
2024-06-05 18:43:35 +0530

    chore: correction

--- repos_main_https.sh
+++ repos_main_https.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ### This script will be used, if keys are not set.
-git clone https://github.com/swaroopanand10/nvim3.git $HOME/.config/nvim
+git clone https://github.com/swaroopanand10/lvim2.git $HOME/.config/nvim
 git clone https://github.com/swaroopanand10/autosetup.git $HOME/.config/autosetup
