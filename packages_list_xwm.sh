3f04834cae5bef19168757c7c23c0ff961541e48
swaroopanand <swaroopanand@email.com>
2024-06-15 23:34:53 +0530

    added another package list for xwms

---
+++ packages_list_xwm.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+packagesParu=(
+
+## xwm
+"xorg"
+"bspwm"
+"xorg-xinit"
+"xclip"
+"xsel"
+"xdotool"
+
+## Zsh packages
+"zsh"
+
+## Wm packages
+"pamixer"
+"playerctl"
+"brightnessctl"
+"udiskie"
+"dunst"
+"network-manager-applet"
+"batsignal"
+
+## Bluetooth
+"bluez"
+"bluez-utils"
+"blueman"
+
+## Terminals
+"kitty"
+
+## Audio related
+"pipewire"
+"pipewire-audio"
+"wireplumber"
+"pipewire-alsa"
+"pipewire-jack"
+"pipewire-pulse"
+"pavucontrol"
+
+## Functionality packages
+"ripgrep"
+"fd"
+"fzf"
+"zoxide"
+"bat"
+"lsd"
+"neovim"
+"starship"
+"tree"
+"imv"
+"dnsmasq" # needed with nmcli
+
+## Media related packages
+"mpc"
+"mpd"
+"ncmpcpp"
+"mpv"
+"yt-dlp"
+"ytfzf"
+"flameshot"
+
+## Zathura related
+"zathura"
+"zathura-pdf-mupdf"
+"zathura-djvu"
+"evince"
+
+## Browsers
+"firefox"
+
+## Fonts
+"ttf-jetbrains-mono-nerd"
+"noto-fonts-emoji" # for emojis
+"noto-fonts" # for emojis
+"ttf-nerd-fonts-symbols" # for non-patched symbols
+"ttf-victor-mono" # victor mono for cursive italics for terminal
+
+## Others
+"stow"
+"tesseract"
+"tesseract-data-eng"
+"chafa"
+"ffmpegthumbnailer"
+"imagemagick"
+"poppler"
+"gparted"
+"polkit-gnome"
+"xournalpp"
+# "visual-studio-code-bin"
+"ansible"
+"unzip"
+"man-db"
+"man-pages"
+"libqalculate"
+"inetutils"
+"rsync"
+
+## Git related
+"git-delta"
+"lazygit"
+"gitui"
+
+## Resource manager
+"htop"
+"bottom"
+"strace"
+
+## Filemanagers
+"lf"
+"yazi"
+"ctpv"
+"nemo"
+
+## language specific packages
+"python"
+"python-pip"
+"python-pynvim"
+"nodejs"
+"npm"
+"gdb"
+"cgdb"
+"kdbg"
+"rustup"
+"go"
+"cmake"
+"luarocks"
+"clang"
+);
