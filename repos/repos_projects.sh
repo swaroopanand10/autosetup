3a0ffb1b4c343a8bfd07081591a848d139b7d907
swaroopanand <swaroopanand@email.com>
2024-07-07 20:38:54 +0530

    chore: small fix

--- repos/repos_projects.sh
+++ repos/repos_projects.sh
@@ -9,7 +9,7 @@ mkdir -p "$HOME/code/os"
 # [ -d "$HOME/code/projects/gtk4tut" ] || git clone git@github.com:swaroopanand10/gtk4tut.git "$HOME/code/projects/gtk4tut"
 # [ -d "$HOME/code/projects/hfc_restart" ] || git clone git@github.com:swaroopanand10/hfc_restart.git "$HOME/code/projects/hfc_restart"
 [ -d "$HOME/code/prg" ] || git clone git@gitlab.com:swaroopanand10/prg.git "$HOME/code/prg"
-[ -d "$HOME/projects/42_piscine" ] || git clone git@github.com:swaroopanand10/42_piscine.git "$HOME/projects/42_piscine"
+[ -d "$HOME/code/projects/42_piscine" ] || git clone git@github.com:swaroopanand10/42_piscine.git "$HOME/code/projects/42_piscine"
 
 # notes
 [ -d "$HOME/notes" ] || git clone git@gitlab.com:swaroopanand10/notes.git "$HOME/notes"
