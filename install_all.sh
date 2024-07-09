a2318efc2552b53674404bc031ead0f2eefd5d7a
swaroopanand <swaroopanand@email.com>
2024-07-09 11:54:14 +0530

    small edit

--- install_all.sh
+++ install_all.sh
@@ -6,9 +6,9 @@ CUR_DIR=$(realpath "$(dirname "$0")")
 
 "$CUR_DIR"/packages/yay_install.sh
 "$CUR_DIR"/packages/install_pack.sh
-# "$CUR_DIR"/dotsetup.sh
+"$CUR_DIR"/dotsetup.sh
 "$CUR_DIR"/plugins/plugins_install.sh
-# "$CUR_DIR"/postinstall.sh
-# "$CUR_DIR"/keygen.sh
+"$CUR_DIR"/postinstall.sh
+"$CUR_DIR"/keygen.sh
 
 ## Repos can't be installed here as they require ssh setup
