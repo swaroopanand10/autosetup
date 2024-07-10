a6567315b3bb56fd1c7eb566263bd4491ccc85de
swaroopanand <swaroopanand@email.com>
2024-07-10 21:01:53 +0530

    minor update

--- plugins/tmux_plugins.sh
+++ plugins/tmux_plugins.sh
@@ -9,6 +9,6 @@ cd "$TMUX_PLUGIN_DIR" || exit
 
 # [ -d 'tmux-resurrect' ] || git clone https://github.com/tmux-plugins/tmux-resurrect
 [ -d 'tmux-fingers' ] || git clone https://github.com/Morantron/tmux-fingers
-[ -d 'tmux-sessionx' ] || git clone https://github.com/omerxx/tmux-sessionx
+# [ -d 'tmux-sessionx' ] || git clone https://github.com/omerxx/tmux-sessionx
 [ -d 'extrakto' ] || git clone https://github.com/laktak/extrakto
 # [ -d 'tmux-copycat' ] || git clone https://github.com/tmux-plugins/tmux-copycat
