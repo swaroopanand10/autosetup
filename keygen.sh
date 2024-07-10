176c23e3cde99b6281a0a5804863008d5ad80d2b
swaroopanand <swaroopanand@email.com>
2024-07-10 21:05:28 +0530

    minor update

--- keygen.sh
+++ keygen.sh
@@ -7,6 +7,6 @@ mkdir -p "$HOME/.ssh"
 ssh-keygen -N '' -f "$HOME/.ssh/id_ed25519"
 
 # copyting the public key into clipboard
-wl-copy < "$HOME/.ssh/id_ed25519.pub"
+# wl-copy < "$HOME/.ssh/id_ed25520.pub" # won't work as wayland session isn't started yet
 
 ## Now paste these ssh keys into respective places.##
